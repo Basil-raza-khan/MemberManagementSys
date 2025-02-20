9c6a09fdfb238e06ec672cb86f68f49e4bf573bb
Mahad <mahadumair26@gmail.com>
2025-02-20 07:11:22 +0500

    Rest ful api is created and the crud operation is performed on array

---
+++ backend/index.js
@@ -0,0 +1,109 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+
+const app = express();
+const portno = 4000;
+const currentTime = new Date();
+// console.log(currentTime);
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let posts = [
+    {
+      id: 1,
+      title: "The Rise of Decentralized Finance",
+      content:
+        "Decentralized Finance (DeFi) is an emerging and rapidly evolving field in the blockchain industry. It refers to the shift from traditional, centralized financial systems to peer-to-peer finance enabled by decentralized technologies built on Ethereum and other blockchains. With the promise of reduced dependency on the traditional banking sector, DeFi platforms offer a wide range of services, from lending and borrowing to insurance and trading.",
+      author: "Alex Thompson",
+      date: "2023-08-01T10:00:00Z",
+    },
+    {
+      id: 2,
+      title: "The Impact of Artificial Intelligence on Modern Businesses",
+      content:
+        "Artificial Intelligence (AI) is no longer a concept of the future. It's very much a part of our present, reshaping industries and enhancing the capabilities of existing systems. From automating routine tasks to offering intelligent insights, AI is proving to be a boon for businesses. With advancements in machine learning and deep learning, businesses can now address previously insurmountable problems and tap into new opportunities.",
+      author: "Mia Williams",
+      date: "2023-08-05T14:30:00Z",
+    },
+    {
+      id: 3,
+      title: "Sustainable Living: Tips for an Eco-Friendly Lifestyle",
+      content:
+        "Sustainability is more than just a buzzword; it's a way of life. As the effects of climate change become more pronounced, there's a growing realization about the need to live sustainably. From reducing waste and conserving energy to supporting eco-friendly products, there are numerous ways we can make our daily lives more environmentally friendly. This post will explore practical tips and habits that can make a significant difference.",
+      author: "Samuel Green",
+      date: "2023-08-10T09:15:00Z",
+    },
+  ];
+
+  let pid = posts.length;
+
+
+app.get("/getData",(req,res)=>{
+    res.send(posts)
+})
+
+app.post('/addData',(req,res)=>{
+
+    const newPost = {
+      id:pid+1,
+      title:req.body.title,
+      content:req.body.content,
+      author:req.body.author,
+      data:currentTime
+    }
+    posts.push(newPost)
+    res.send({message:"successfully added",newPost})
+    
+})
+
+app.put('/updateData/:Data_id',(req,res)=>{
+
+  let pid = parseInt(req.params.Data_id);
+
+  let targetIndex = posts.findIndex((o)=>{
+    return o.id === pid
+  })
+
+  if(targetIndex !== -1){
+      posts[targetIndex] = {
+      id:target,
+      title:req.body.title,
+      content:req.body.content,
+      author:req.body.author,
+      date:currentTime
+    }
+
+    res.send(posts[targetIndex])
+
+  }else{
+    res.send("id not found")
+  }
+  
+  
+})
+
+app.delete("/deleteData/:Data_id",(req,res)=>{
+
+  let pid = parseInt(req.params.Data_id);
+  let targetIndex = posts.findIndex((o)=>{
+    return o.id === pid
+  })
+
+  if(targetIndex !== -1){
+    posts.splice(targetIndex,1)
+    res.send({
+      message:`successfully deleted data from id ${pid}`,
+      updatedData:posts
+      });
+  }else{
+    res.send("id not found")
+  }  
+})
+
+
+
+app.listen(portno,()=>{
+  console.log(`server running on port ${portno}`);
+})
